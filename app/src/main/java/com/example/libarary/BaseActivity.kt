48d8f7436e9128a1acc9c14ba45b133721d3bf44
mykim76 <iyoungfm@gmail.com>
2020-06-09 20:13:08 +0900

    사진크게보기 화면 만들어서 써클이미지에서 옮겨가기 -> 핀치 줌/ 더블터치 시 확대/축소 대응이 필요함

--- app/src/main/java/com/example/libarary/BaseActivity.kt
+++ app/src/main/java/com/example/libarary/BaseActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity:AppCompatActivity() {
 
+    val mContent = this
     abstract fun setValues()
     abstract fun setupEvents()
 }
--- app/src/main/java/com/example/libarary/LargePhotoActivity.kt
+++ app/src/main/java/com/example/libarary/LargePhotoActivity.kt
@@ -1,18 +1,16 @@
 package com.example.libarary
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity() {
+class LargePhotoActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_large_photo)
         setValues()
         setupEvents()
+
     }
 
     override fun setValues() {
@@ -21,10 +19,5 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
-        profilePhoto.setOnClickListener {
-
-            val myIntent = Intent(mContent,LargePhotoActivity::class.java)
-            startActivity(myIntent)
-        }
     }
 }
